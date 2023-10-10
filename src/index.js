a1c2ab5597ee467e436036c376fdf86a9791314e
Domas <114532410+Domukas22@users.noreply.github.com>
2023-10-10 19:03:36 +0200

    Starter file with ESLinst and Prettier

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import returnText from "./logic/function";
 import "./styles/reset.css";
 import "./styles/main.css";
 
-function _print_this(textSTRING) {
+function print(textSTRING) {
   const text = document.createElement("h2");
   text.innerHTML = textSTRING;
   document.body.appendChild(text);
 }
 
-_print_this(returnText());
+print(returnText());
+print("hello there");
